935bfd55d15b3b1aed0011ab3a275f5eec53bcb9
omni <omni@omni-desktop@mrrobot.com>
2019-10-31 16:29:12 +0530

    setting and retreiving user meta data

--- sources/apps/app/objectDetector_mobilenetSSD/deepstream_app_ssd.cpp
+++ sources/apps/app/objectDetector_mobilenetSSD/deepstream_app_ssd.cpp
@@ -57,6 +57,11 @@
  * based on the fastest source's framerate. */
 #define MUXER_BATCH_TIMEOUT_USEC 4000000
 
+#define USER_ARRAY_SIZE 16
+
+/** set the user metadata type */
+#define NVDS_USER_FRAME_META_EXAMPLE (nvds_get_user_meta_type("NVIDIA.NVINFER.USER_META"))
+
 /* NVIDIA Decoder source pad memory feature. This feature signifies that source
  * pads having this capability will push GstBuffers containing cuda buffers. */
 #define GST_CAPS_FEATURES_NVMM "memory:NVMM"
@@ -67,15 +72,50 @@ const gchar pgie_classes_str[PGIE_DETECTED_CLASS_NUM][32] =
 
 char* rtsp_links[] = { "NULL", "rtsp://admin:admin123@192.168.0.102", "rtsp://admin:admin123@192.168.0.103/Streaming/Channels/101", "rtsp://admin:admin123@192.168.0.101/Streaming/Channels/101", NULL };
 
-
 // #define FPS_PRINT_INTERVAL 300
 
+void *set_metadata_ptr(void);
+static gpointer copy_user_meta(gpointer data, gpointer user_data);
+static void release_user_meta(gpointer data, gpointer user_data);
+
+void *set_metadata_ptr()
+{
+  int i = 0;
+  gchar *user_metadata = (gchar*)g_malloc0(USER_ARRAY_SIZE);
+
+  g_print("\n**************** Setting user metadata array of 16 on nvinfer src pad\n");
+  for(i = 0; i < USER_ARRAY_SIZE; i++) {
+    user_metadata[i] = rand() % 255;
+    g_print("user_meta_data [%d] = %d\n", i, user_metadata[i]);
+  }
+  return (void *)user_metadata;
+}
+
+/* copy function set by user. "data" holds a pointer to NvDsUserMeta*/
+static gpointer copy_user_meta(gpointer data, gpointer user_data)
+{
+  NvDsUserMeta *user_meta = (NvDsUserMeta *)data;
+  gchar *src_user_metadata = (gchar*)user_meta->user_meta_data;
+  gchar *dst_user_metadata = (gchar*)g_malloc0(USER_ARRAY_SIZE);
+  memcpy(dst_user_metadata, src_user_metadata, USER_ARRAY_SIZE);
+  return (gpointer)dst_user_metadata;
+}
+
+/* release function set by user. "data" holds a pointer to NvDsUserMeta*/
+static void release_user_meta(gpointer data, gpointer user_data)
+{
+  NvDsUserMeta *user_meta = (NvDsUserMeta *) data;
+  if(user_meta->user_meta_data) {
+    g_free(user_meta->user_meta_data);
+    user_meta->user_meta_data = NULL;
+  }
+}
 /* This is the buffer probe function that we have registered on the sink pad
  * of the OSD element. All the infer elements in the pipeline shall attach
  * their metadata to the GstBuffer, here we will iterate & process the metadata
  * forex: class ids to strings, counting of class_id objects etc. */
 static GstPadProbeReturn
-tiler_src_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info,
+osd_sink_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info,
     gpointer u_data)
 {
   GstBuffer *buf = (GstBuffer *) info->data;
@@ -84,15 +124,19 @@ tiler_src_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info,
   guint person_count = 0;
   NvDsMetaList *l_frame = NULL;
   NvDsMetaList *l_obj = NULL;
+  guint source_id;
   NvDsDisplayMeta *display_meta = NULL;
   gchar *msg = NULL;
+  NvDsMetaList * l_user_meta = NULL;
+  NvDsUserMeta *user_meta = NULL;
+  gchar *user_meta_data = NULL;
+  int i = 0;
 
   NvDsBatchMeta *batch_meta = gst_buffer_get_nvds_batch_meta (buf);
-
   for (l_frame = batch_meta->frame_meta_list; l_frame != NULL;
       l_frame = l_frame->next) {
+    display_meta = nvds_acquire_display_meta_from_pool (batch_meta);
     NvDsFrameMeta *frame_meta = (NvDsFrameMeta *) (l_frame->data);
-    int offset = 0;
     for (l_obj = frame_meta->obj_meta_list; l_obj != NULL; l_obj = l_obj->next) {
       obj_meta = (NvDsObjectMeta *) (l_obj->data);
       if (obj_meta->class_id == PGIE_CLASS_ID_PERSON) {
@@ -100,9 +144,34 @@ tiler_src_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info,
         num_rects++;
       }
     }
-    display_meta = nvds_acquire_display_meta_from_pool (batch_meta);
+    for (l_user_meta = frame_meta->frame_user_meta_list; l_user_meta != NULL;
+        l_user_meta = l_user_meta->next) {
+      user_meta = (NvDsUserMeta *) (l_user_meta->data);
+      user_meta_data = (gchar *)user_meta->user_meta_data;
+
+      if(user_meta->base_meta.meta_type == NVDS_USER_FRAME_META_EXAMPLE)
+      {
+        g_print("\n************ Retrieving user_meta_data array of 16 on osd sink pad\n");
+        for(i = 0; i < USER_ARRAY_SIZE; i++) {
+          g_print("user_meta_data [%d] = %d\n", i, user_meta_data[i]);
+        }
+        g_print("\n");
+      }
+    }
     NvOSD_TextParams *txt_params = &display_meta->text_params[0];
     display_meta->num_labels = 1;
+    NvOSD_LineParams *line_params  = display_meta->line_params;
+    int offset = 0;
+    source_id = frame_meta->source_id;
+    g_print("Source: %d", source_id);
+    line_params[0].x1 = 400;//for demonstration, user need to se these values
+    line_params[0].y1 = 400;
+    line_params[0].x2 = 800;
+    line_params[0].y2 = 800;
+    line_params[0].line_width = 4;
+    line_params[0].line_color = (NvOSD_ColorParams){1.0, 0.0, 0.0, 1.0};
+    display_meta->num_lines++;
+    g_print("END %d\n", source_id);
     txt_params->display_text = (gchar *) g_malloc0 (MAX_DISPLAY_LEN);
     offset =
         snprintf (txt_params->display_text, MAX_DISPLAY_LEN, "Person = %d ",
@@ -126,10 +195,8 @@ tiler_src_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info,
     txt_params->text_bg_clr.green = 0.0;
     txt_params->text_bg_clr.blue = 0.0;
     txt_params->text_bg_clr.alpha = 1.0;
-
     nvds_add_display_meta_to_frame (frame_meta, display_meta);
   }
-
   // g_print ("Frame Number = %d"
   //     "Person Count = %d\n",
   //     frame_number, person_count);
@@ -167,7 +234,8 @@ pgie_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info, gpointer u_data)
   0.2, 0.2}};
   static float groupThreshold = 1;
   static float groupEps = 0.2;
-
+  NvDsUserMeta *user_meta = NULL;
+  NvDsMetaType user_meta_type = NVDS_USER_FRAME_META_EXAMPLE;
   NvDsBatchMeta *batch_meta =
       gst_buffer_get_nvds_batch_meta (GST_BUFFER (info->data));
 
@@ -176,13 +244,24 @@ pgie_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info, gpointer u_data)
       l_frame = l_frame->next) {
     NvDsFrameMeta *frame_meta = (NvDsFrameMeta *) l_frame->data;
 
+    /* Acquire NvDsUserMeta user meta from pool */
+    user_meta = nvds_acquire_user_meta_from_pool(batch_meta);
+    /* Set NvDsUserMeta below */
+    user_meta->user_meta_data = (void *)set_metadata_ptr();
+    user_meta->base_meta.meta_type = user_meta_type;
+    user_meta->base_meta.copy_func = (NvDsMetaCopyFunc)copy_user_meta;
+    user_meta->base_meta.release_func = (NvDsMetaReleaseFunc)release_user_meta;
+              
+    /* We want to add NvDsUserMeta to frame level */
+    nvds_add_user_meta_to_frame(frame_meta, user_meta); 
+
     /* Iterate user metadata in frames to search PGIE's tensor metadata */
     for (NvDsMetaList * l_user = frame_meta->frame_user_meta_list;
         l_user != NULL; l_user = l_user->next) {
       NvDsUserMeta *user_meta = (NvDsUserMeta *) l_user->data;
       if (user_meta->base_meta.meta_type != NVDSINFER_TENSOR_OUTPUT_META)
         continue;
-
+            
       /* convert to tensor metadata */
       NvDsInferTensorMeta *meta =
           (NvDsInferTensorMeta *) user_meta->user_meta_data;
@@ -410,7 +489,7 @@ main (int argc, char *argv[])
 #endif
   GstBus *bus = NULL;
   guint bus_watch_id = 0;
-  GstPad *osd_sink_pad = NULL, *tiler_src_pad = NULL;
+  GstPad *osd_sink_pad = NULL, *tiler_sink_pad = NULL;
   guint i;
   guint pgie_batch_size;
   guint tiler_rows;
@@ -573,15 +652,24 @@ gst_bin_add_many (GST_BIN (pipeline), pgie, tiler, nvvidconv, nvosd, sink,
   }
 #endif
 
-  /* Lets add probe to get informed of the meta data generated, we add probe to
+  /* Add probe to get informed of the meta data generated, we add probe to
    * the sink pad of the osd element, since by that time, the buffer would have
    * had got all the metadata. */
-  tiler_src_pad = gst_element_get_static_pad (pgie, "src");
-  if (!tiler_src_pad)
-    g_print ("Unable to get src pad\n");
+  osd_sink_pad = gst_element_get_static_pad (nvosd, "sink");
+  if (!osd_sink_pad)
+    g_print ("Unable to get sink pad\n");
   else
-    gst_pad_add_probe (tiler_src_pad, GST_PAD_PROBE_TYPE_BUFFER,
-        tiler_src_pad_buffer_probe, (gpointer)sink, NULL);
+    gst_pad_add_probe (osd_sink_pad, GST_PAD_PROBE_TYPE_BUFFER,
+        osd_sink_pad_buffer_probe, NULL, NULL);
+
+  /* Add probe to get informed of the meta data generated, we add probe to
+   * the sink pad of tiler element which is just after all SGIE elements.
+   * Since by that time, GstBuffer would have had got all SGIEs tensor
+   * metadata. */
+  tiler_sink_pad = gst_element_get_static_pad (tiler, "sink");
+  gst_pad_add_probe (tiler_sink_pad, GST_PAD_PROBE_TYPE_BUFFER,
+      pgie_pad_buffer_probe, NULL, NULL);
+
   /* Set the pipeline to "playing" state */
   g_print ("Now playing:");
   for (i = 0; i < num_sources; i++) {
